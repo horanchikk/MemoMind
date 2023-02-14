f7fc7ed1e8cc1d9a4d93c5ac8494aef561a250b2
Ethosa <social.ethosa@gmail.com>
2023-02-14 16:26:02 +0700

    update database

--- backend/database.py
+++ backend/database.py
@@ -35,7 +35,7 @@ DeskCardLabel:
     id [integer]
     title [string]
     color [string]  # HEX string
-Property:
+DeskProperty:
     id [integer]
     ptype [integer]  # ID of property type
     object [object]  # object data
@@ -48,4 +48,11 @@ from pymongo import MongoClient
 
 db = MongoClient('localhost', 27017)
 collection = db.get_database('MemoMind')
-uc = collection['user']
+user = collection['user']
+note = collection['note']
+desk = collection['desk']
+desk_column = collection['desk_column']
+desk_card = collection['desk_card']
+desk_card_label = collection['desk_card_label']
+desk_property = collection['desk_property']
+property_type = collection['property_type']
