a8121a26d07e3e1f7f0bc60d4a428b0a88f9528f
Ethosa <social.ethosa@gmail.com>
2023-02-16 02:38:26 +0700

    fix .. again

--- backend/mounts/desk.py
+++ backend/mounts/desk.py
@@ -265,7 +265,7 @@ async def move_card(
         }}}
     )
     d = desk.find_one({'did': did})
-    return {'response': DeskModel(**d).dict()}
+    return {'response': 'success'}
 
 
 @desk_app.patch('/id{did}/column{cid}/card{card_index}')
